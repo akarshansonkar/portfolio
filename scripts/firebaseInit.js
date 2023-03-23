5cb223a1beba45b1d97f568554a9cdef2f5cd787
akarshan sonkar <66207403+akarshansonkar@users.noreply.github.com>
2023-03-23 14:04:54 +0530

    Add files via upload

---
+++ scripts/firebaseInit.js
@@ -0,0 +1,13 @@
+/*-------------------------------------------
+            INTIALIZING FIREBASE
+---------------------------------------------*/
+
+// TODO: Replace the following with your app's Firebase project configuration.
+const firebaseConfig = {
+  // The value of `databaseURL` depends on the location of the database.
+  databaseURL: "https://portfolio-374f4-default-rtdb.firebaseio.com",
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const ref = firebase.database().ref();
